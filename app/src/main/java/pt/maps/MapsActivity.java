1a2d59c796e5d327639ab9dd45d1a8229e894952
t5topa00 <t5topa00@stundents.oamk.fi>
2017-03-20 20:59:33 +0200

    gfgd

--- app/src/main/java/pt/maps/MapsActivity.java
+++ app/src/main/java/pt/maps/MapsActivity.java
@@ -25,7 +25,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .findFragmentById(R.id.map);
                 mapFragment.getMapAsync(this);
             }
-
+        //Pasi testi
 
             //adfadsfdadfads
         //asdfadfa
